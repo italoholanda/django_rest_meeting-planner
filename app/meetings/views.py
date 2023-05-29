b0b93dae0614396516bee3d74d3c395503bbc18e
italo <italo.holanda@escolaconquer.com.br>
2023-05-29 15:31:23 -0300

    feat: put and delete methods

--- app/meetings/views.py
+++ app/meetings/views.py
@@ -5,12 +5,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def meetings(request):
     if request.method == "GET":
         objects = Meeting.objects.all()
         serializer = MeetingSerializer(objects, many=True)
         return Response(serializer.data)
+
     elif request.method == "POST":
         serializer = MeetingSerializer(data=request.data)
         if serializer.is_valid():
@@ -18,8 +19,30 @@ def meetings(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    elif request.method == 'PUT':
+        meeting_id = int(request.query_params.get('id'))
 
-@api_view(['GET', 'POST'])
+        try:
+            meeting = Meeting.objects.get(pk=meeting_id)
+            serializer = MeetingSerializer(meeting, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Meeting.DoesNotExist:
+            return Response("Meeting not found", status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        meeting_id = request.query_params.get('id')
+        meeting = Meeting.objects.filter(pk=int(meeting_id)).first()
+        if meeting != None:
+            meeting.delete()
+            return Response(status=status.HTTP_200_OK)
+        return Response("Meeting not found", status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def rooms(request):
     if request.method == "GET":
         objects = Room.objects.all()
@@ -31,3 +54,25 @@ def rooms(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PUT':
+        room_id = int(request.query_params.get('id'))
+
+        try:
+            room = Room.objects.get(pk=room_id)
+            serializer = RoomSerializer(room, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Room.DoesNotExist:
+            return Response("Room not found", status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        room_id = request.query_params.get('id')
+        room = Room.objects.filter(pk=int(room_id)).first()
+        if room != None:
+            room.delete()
+            return Response(status=status.HTTP_200_OK)
+        return Response("Meeting not found", status=status.HTTP_400_BAD_REQUEST)
