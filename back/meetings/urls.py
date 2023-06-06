bd7b72ff0e6ec693bc34b574ca141b70733abcaf
italo <italo.holanda@escolaconquer.com.br>
2023-06-06 19:30:01 -0300

    feat: added path-names

--- back/meetings/urls.py
+++ back/meetings/urls.py
@@ -3,9 +3,9 @@ from .views import get_meetings, get_meeting, create_meeting, update_meeting, de
 
 
 urlpatterns = [
-    path('', get_meetings),
-    path('<int:id>/', get_meeting),
-    path('create/', create_meeting),
-    path('update/', update_meeting),
-    path('delete/', delete_meeting),
+    path('', get_meetings, name="get_meetings"),
+    path('<int:id>/', get_meeting, name="get_meeting"),
+    path('create/', create_meeting, name="create_meeting"),
+    path('update/', update_meeting, name="update_meeting"),
+    path('delete/', delete_meeting, name="delete_meeting"),
 ]
