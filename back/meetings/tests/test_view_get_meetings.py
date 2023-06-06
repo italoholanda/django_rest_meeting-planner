915e99f3f6217355e52513b259ce916b9611a976
italo <italo.holanda@escolaconquer.com.br>
2023-06-06 19:28:48 -0300

    test: get_meetings views tests

---
+++ back/meetings/tests/test_view_get_meetings.py
@@ -0,0 +1,18 @@
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+from .test_view import TestView
+
+
+class TestGetMeetings(TestView):
+
+    def test_get_meetings_should_return_200_if_authenticated(self):
+        path = reverse("get_meetings")
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_meetings_should_return_401_if_un_authenticated(self):
+        path = reverse("get_meetings")
+        un_authenticated_client = APIClient()
+        response = un_authenticated_client.get(path)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
